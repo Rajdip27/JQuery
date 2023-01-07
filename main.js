640d6bc519640bef110bb136b7fbf017cc716923
Raj <srajdip920@gmail.com>
2023-01-08 01:02:00 +0600

    JQuery

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+     $('button').click(function(){
+        // $('a').hide();
+        // $('[href]').hide();
+        // $(':Button').hide();
+        $('a[target="_blank"]').hide();
+
+     })
+
+
+});
